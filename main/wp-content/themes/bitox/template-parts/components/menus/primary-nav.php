9aef0d2e8895d9b30c689c1a0b78d7873f6700cb
Vyacheslav Tyutyunkov <tve@softmotions.com>
2018-05-02 16:36:47 +0700

    wp removed from repo

--- main/wp-content/themes/bitox/template-parts/components/menus/primary-nav.php
+++
@@ -1,9 +0,0 @@
-<?php if ( has_nav_menu( 'primary' ) ) : ?>
-  <nav id="primary-nav" class="primary-nav ml-lg-auto col-12 col-lg-auto" role="navigation">
-    <?php wp_nav_menu( array(
-      'theme_location' => 'primary',
-      'menu_class' => 'primary-nav__list clearlist',
-      'container' => false
-    ) ); ?>
-  </nav>
-<?php endif; ?>
--- main/wp-content/themes/bitox/template-parts/sidebar.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-	return;
-}
-?>
-
-<aside class="widget-area">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
-</aside>
